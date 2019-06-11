bcdd2e378d59b9b0b31f40f2728bd9c9f16750f7
Angel Gustavo Ek Tun <al1609044@upy.edu.mx>
2019-06-11 11:52:14 -0500

    final robot arm

--- controllers/robot2/robot2.c
+++ controllers/robot2/robot2.c
@@ -36,7 +36,11 @@ int main(int argc, char **argv)
    *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
    *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
    */
-   WbDeviceTag hola = wb_robot_get_device("servo_motor");
+   WbDeviceTag servo= wb_robot_get_device("servo_motor");
+   WbDeviceTag frame_1 =wb_robot_get_device("motor_frame_1");
+   WbDeviceTag frame_2 =wb_robot_get_device("motor_frame_2");
+   WbDeviceTag frame_3 =wb_robot_get_device("motor_frame_3");
+
   /* main loop
    * Perform simulation steps of TIME_STEP milliseconds
    * and leave the loop when the simulation is over
@@ -56,7 +60,13 @@ int main(int argc, char **argv)
      * wb_differential_wheels_set_speed(100.0,100.0);
      */
 
-    wb_motor_set_position(hola,PI/4);
+    wb_motor_set_position(servo,PI/4);
+    wb_motor_set_position(frame_1,-PI/4);
+    wb_motor_set_available_force(frame_1,50000);
+    wb_motor_set_position(frame_2,-PI/6);
+    wb_motor_set_available_force(frame_2,50000);
+    wb_motor_set_position(frame_3,-PI/8);
+    wb_motor_set_available_force(frame_3,50000);
   };
 
   /* Enter your cleanup code here */
